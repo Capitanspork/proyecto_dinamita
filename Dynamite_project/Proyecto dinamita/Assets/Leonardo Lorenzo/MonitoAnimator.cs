c5861bd6ee984783efe99f8c4c42e313ec3238bf
VastoGrim <65916224+VastoGrim@users.noreply.github.com>
2021-07-23 11:02:18 -0500

    Idle GameObject
    se añadio el codigo para tener un GO idle en el animador.

--- Dynamite_project/Proyecto dinamita/Assets/Leonardo Lorenzo/MonitoAnimator.cs
+++ Dynamite_project/Proyecto dinamita/Assets/Leonardo Lorenzo/MonitoAnimator.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class MonitoAnimator : MonoBehaviour
 {
     public GameObject[] baileAnimSprites;
+    public GameObject mainMonito;
+    private bool animated;
     
 
     // Update is called once per frame
@@ -14,50 +16,69 @@ public class MonitoAnimator : MonoBehaviour
         monitoRight();
         monitoLeft();
         monitoDown();
+        if (animated == false)
+        {
+            mainMonito.gameObject.SetActive(true);
+        }
     }
 
     public void monitoLeft()
     {
         if (Input.GetKey(KeyCode.Q))
         {
+            animated = true;
+            mainMonito.gameObject.SetActive(false);
             baileAnimSprites[0].gameObject.SetActive(true);
         }
-        else
+        if (Input.GetKeyUp(KeyCode.Q))
+        {
+            animated = false;
             baileAnimSprites[0].gameObject.SetActive(false);
-
+        } 
     }
 
     public void monitoRight()
     {
         if (Input.GetKey(KeyCode.P))
         {
+            animated = true;
+            mainMonito.gameObject.SetActive(false);
             baileAnimSprites[1].gameObject.SetActive(true);
         }
-        else
+        if (Input.GetKeyUp(KeyCode.P))
+        {
+            animated = false;
             baileAnimSprites[1].gameObject.SetActive(false);
-
+        }  
     }
 
     public void monitoUp()
     {
         if (Input.GetKey(KeyCode.W))
         {
+            animated = true;
+            mainMonito.gameObject.SetActive(false);
             baileAnimSprites[2].gameObject.SetActive(true);
         }
-        else
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            animated = false;
             baileAnimSprites[2].gameObject.SetActive(false);
-
+        }      
     }
 
     public void monitoDown()
     {
         if (Input.GetKey(KeyCode.O))
         {
+            animated = true;
+            mainMonito.gameObject.SetActive(false);
             baileAnimSprites[3].gameObject.SetActive(true);
         }
-        else
+        if (Input.GetKeyUp(KeyCode.O))
+        {
+            animated = false;
             baileAnimSprites[3].gameObject.SetActive(false);
-
+        } 
     }
-
 }
