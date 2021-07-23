218d2360960d5163af6e0610d82b7f81b944a5c7
VastoGrim <65916224+VastoGrim@users.noreply.github.com>
2021-07-23 11:20:09 -0500

    Animator hotfix
    ya no se pueden stackear animaciones si se aprieta más de una tecla al mismo tiempo.

--- Dynamite_project/Proyecto dinamita/Assets/Leonardo Lorenzo/MonitoAnimator.cs
+++ Dynamite_project/Proyecto dinamita/Assets/Leonardo Lorenzo/MonitoAnimator.cs
@@ -6,7 +6,7 @@ public class MonitoAnimator : MonoBehaviour
 {
     public GameObject[] baileAnimSprites;
     public GameObject mainMonito;
-    private bool animated;
+    private bool animated,beingAnimated;
     
 
     // Update is called once per frame
@@ -24,60 +24,69 @@ public class MonitoAnimator : MonoBehaviour
 
     public void monitoLeft()
     {
-        if (Input.GetKey(KeyCode.Q))
+        if (Input.GetKey(KeyCode.Q) && beingAnimated == false)
         {
             animated = true;
+            beingAnimated = true;
             mainMonito.gameObject.SetActive(false);
             baileAnimSprites[0].gameObject.SetActive(true);
+           
         }
         if (Input.GetKeyUp(KeyCode.Q))
         {
             animated = false;
+            beingAnimated = false;
             baileAnimSprites[0].gameObject.SetActive(false);
         } 
     }
 
     public void monitoRight()
     {
-        if (Input.GetKey(KeyCode.P))
+        if (Input.GetKey(KeyCode.P) && beingAnimated == false)
         {
             animated = true;
+            beingAnimated = true;
             mainMonito.gameObject.SetActive(false);
             baileAnimSprites[1].gameObject.SetActive(true);
         }
         if (Input.GetKeyUp(KeyCode.P))
         {
             animated = false;
+            beingAnimated = false;
             baileAnimSprites[1].gameObject.SetActive(false);
         }  
     }
 
     public void monitoUp()
     {
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.W) && beingAnimated == false)
         {
             animated = true;
+            beingAnimated = true;
             mainMonito.gameObject.SetActive(false);
             baileAnimSprites[2].gameObject.SetActive(true);
         }
         if (Input.GetKeyUp(KeyCode.W))
         {
             animated = false;
+            beingAnimated = false;
             baileAnimSprites[2].gameObject.SetActive(false);
         }      
     }
 
     public void monitoDown()
     {
-        if (Input.GetKey(KeyCode.O))
+        if (Input.GetKey(KeyCode.O) && beingAnimated == false)
         {
             animated = true;
+            beingAnimated = true;
             mainMonito.gameObject.SetActive(false);
             baileAnimSprites[3].gameObject.SetActive(true);
         }
         if (Input.GetKeyUp(KeyCode.O))
         {
             animated = false;
+            beingAnimated = false;
             baileAnimSprites[3].gameObject.SetActive(false);
         } 
     }
